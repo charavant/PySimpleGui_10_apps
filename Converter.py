21533fd5c5945861a870e47360450c89d3d52fc9
charavant <charavant@gmail.com>
2022-10-25 23:42:19 +0300

    base

--- Converter.py
+++ Converter.py
@@ -2,7 +2,14 @@ import PySimpleGUI as sg
 
 
 layout = [
-    []
+    [
+        sg.Input(key='-INPUT-'),
+        sg.Spin(['km to mile','kg to pound','sec to min'], key = '-UNITS-'), 
+        sg.Button('Convert', key = '-CONVERT-')
+    ],
+    [
+        sg.Text('Output', key = '-OUTPUT-')
+    ]
 ]
 
 window = sg.Window('Converter', layout)
@@ -12,6 +19,12 @@ while True:
     
     if event == sg.WIN_CLOSED:
         break
-    
- 
+    if event == '-CONVERT-':
+        input_value = values['-INPUT-']
+        if input_value.isnumeric():
+            match values['-UNITS-']:
+              case 'km to mile':
+                  output = input_value * 0.6214
+       
+        
 window.close()
