5a9ba2398b78393821b1dfc2f33c4dfb2210a72b
charavant <charavant@gmail.com>
2022-10-26 19:12:00 +0300

    new

--- calculator.py
+++ calculator.py
@@ -1,14 +1,19 @@
-
 import PySimpleGUI as sg
 
+sg.theme('LightGrey1')
+sg.set_options(font= 'Franklin 14', button_element_size= (6,3))
+button_size = (6,3)
 layout = [
-    [sg.Text('Output', key = '-OUTPUT-')],
-    [sg.Button('Clear'),sg.Button('Enter')],
-    [sg.Button(),sg.Button(),sg.Button(),sg.Button()],
-    [sg.Button(),sg.Button(),sg.Button(),sg.Button()],
-    [sg.Button(),sg.Button(),sg.Button(),sg.Button()],
-    [sg.Button(),sg.Button(),sg.Button()]
+    [sg.Text('Output', font = 'Franklin 26', justification= 'right', expand_x= True)],
+    
+    [sg.Button('Clear', expand_x= True),sg.Button('Enter', expand_x= True)],
+    [sg.Button(7, size=button_size),sg.Button(8, size=button_size),sg.Button(9, size=button_size),sg.Button('*', size=button_size)],
+    [sg.Button(4, size=button_size),sg.Button(5, size=button_size),sg.Button(6, size=button_size),sg.Button('/', size=button_size)],
+    [sg.Button(1, size=button_size),sg.Button(2, size=button_size),sg.Button(3, size=button_size),sg.Button('-', size=button_size)],
+    [sg.Button(0, expand_x= True),sg.Button('.', size=button_size),sg.Button('+', size=button_size)]
 ]
+
+
 
 window = sg.Window('Calculator', layout)
 
