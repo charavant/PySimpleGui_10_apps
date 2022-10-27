41bd275110b00ccabbb6df2a8c50ca925e7af653
charavant <charavant@gmail.com>
2022-10-27 10:57:14 +0300

    new app

---
+++ stopwatch.py
@@ -0,0 +1,25 @@
+import PySimpleGUI as sg 
+
+sg.theme('black')
+layout = [
+    [sg.Image()],
+    [sg.VPush()],
+    [sg.Text('time')],
+    [sg.Button('Start'), sg.Button('Lap')],
+    [sg.VPush()]
+]
+
+window = sg.Window(
+    'Stopwatch',
+    layout,
+    size = (300,300),
+    no_titlebar= True,
+    element_justification='center'
+)
+
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Start'):
+        break
+    
+window.close()
